c6ace518c734f029ae0673485a97c496b699cebe
danielDeVita <danielitodevita@gmail.com>
2023-10-16 17:31:33 -0300

    updated swagger

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
+import { ApiTags, ApiExcludeController } from '@nestjs/swagger';
 
+@ApiExcludeController()
+@ApiTags('Cart')
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,33 +1,64 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Order')
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a new order',
+    description: 'This api used to create a new order in the database',
+  })
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.orderService.create(createOrderDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Find all orders',
+    description: 'This API is used for fetching all orders',
+  })
   findAll() {
     return this.orderService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Find an order by id',
+    description:
+      'this api is used to get details of particular order by its ID',
+  })
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update an existing order',
+    description: 'This API is used update an existing order',
+  })
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.orderService.update(id, updateOrderDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete an existing order',
+    description: 'this api delete single product',
+  })
   remove(@Param('id') id: string) {
     return this.orderService.remove(id);
   }
