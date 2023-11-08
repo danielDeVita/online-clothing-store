0c78a17f9a7ab1ef6ea59cb39e68dc9bc437feba
danielDeVita <danielitodevita@gmail.com>
2023-11-08 17:40:54 -0300

    tests for auth module done

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { RegisterAuthDto } from './dto/register-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 describe('AuthController', () => {
   let controller: AuthController;
+
+  const mockAuthService = {
+    register: jest.fn((dto: RegisterAuthDto) => dto),
+    login: jest.fn((dto: LoginAuthDto) => ({
+      user: { email: dto.email, password: dto.password },
+      token: 'some token',
+    })),
+  };
+
+  const mockRegisterDto: RegisterAuthDto = {
+    name: 'test name',
+    password: 'test Password',
+    email: 'testMail@gmail.com',
+  };
+  const mockLoginDto: LoginAuthDto = { password: 'test Password', email: 'testMail@gmail.com' };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [AuthService],
-    }).compile();
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockAuthService)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should register a new user', () => {
+    expect(controller.registerUser(mockRegisterDto)).toEqual({
+      password: 'test Password',
+      email: 'testMail@gmail.com',
+      name: 'test name',
+    });
+  });
+
+  it('should login a user', () => {
+    expect(controller.loginUser(mockLoginDto)).toEqual({
+      user: { email: 'testMail@gmail.com', password: 'test Password' },
+      token: 'some token',
+    });
   });
 });
